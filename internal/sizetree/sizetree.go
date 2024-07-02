93c593973f5ab5cedb4dfa9f4165b8e97bddb14b
jesse galley <jesse.galley@gmail.com>
2024-07-02 14:42:04 -0300

    finished tests for sizetree

--- internal/sizetree/sizetree.go
+++ internal/sizetree/sizetree.go
@@ -16,6 +16,65 @@ func New() *SizeTree  {
   }
 }
 
+// ReplaceOrInsert inserts a SizeTreeEntry into the SizeTree, returning nil if 
+// a SizeTreeEntry of that size did not exist, and returning the existing SizeTreeEntry
+// if one of that size already existed.
+func (s *SizeTree) ReplaceOrInsert (e *SizeTreeEntry) *SizeTreeEntry {
+  node := s.btree.ReplaceOrInsert(e)
+  if node == nil{
+    return nil
+  }
+
+  return node.(*SizeTreeEntry)
+}
+
+// MergeOrInsert inserts he given SizeTreeEntry into the SizeTree 
+// if an entry of that Size already exists, the two entries are merged
+// returns nil if no merge occured
+func (s *SizeTree) MergeOrInsert (e *SizeTreeEntry) *SizeTreeEntry {
+  item := s.btree.ReplaceOrInsert(e)
+  if item == nil {
+    return nil
+  }
+
+  // item and e are different, meaning the Size already existed in the tree 
+  // merge the two SizeTreeEntries
+  item.(*SizeTreeEntry).Merge(e)
+
+  // re-insert the merged entry 
+  item2 := s.btree.ReplaceOrInsert(item)
+  if item2 ==  nil {
+    // we should never get here 
+    panic("something when horribly wrong trying to merge an entry")
+  }
+
+  // finally return the merged entry back to the caller
+  return item.(*SizeTreeEntry)
+}
+
+// Get returns a pointer to a SizeTreeEntry of matching Size from the SizeTree, 
+// if no entry of this size  exists then it returns nil.
+func (s *SizeTree) Get(e *SizeTreeEntry) *SizeTreeEntry {
+  item := s.btree.Get(e)
+  if item == nil {
+    return nil
+  }
+
+  return item.(*SizeTreeEntry)
+}
+
+// GetBySize returns a pointer to a SizeTreeEntry of matching Size from the SizeTree,
+// if no entry of this size  exists then it returns nil.
+func (s *SizeTree) GetBySize(size int64) *SizeTreeEntry {
+  e := NewSizeTreeEntry(size, nil)
+  item := s.btree.Get(e)
+  if item == nil {
+    return nil
+  }
+
+  return item.(*SizeTreeEntry)
+}
+
 // SizeTreeEntry is a single node in the SizeTree btree structure. It is indexed
 // on the size of the file, and contains a slice of files which share this size.
 type SizeTreeEntry struct {
@@ -34,6 +93,27 @@ func NewSizeTreeEntry (size int64, files []string) *SizeTreeEntry {
 // Append() method appends a file to the SizeTreeEntry.
 func (s *SizeTreeEntry) Append(file string) {
   s.files = append(s.files, file)
+}
+
+// Merge method appends all files from the other SizeTreeEntry into this one.
+func (s *SizeTreeEntry) Merge(other *SizeTreeEntry) {
+  otherFiles := other.Files() 
+  if otherFiles == nil {
+    return
+  }
+
+  s.files = append(s.files, otherFiles...)
+}
+
+// Files retuns a []string of file paths associated with this SizeTreeEntry 
+func (s *SizeTreeEntry) Files() []string {
+  if len(s.files) == 0 {
+    return nil
+  } else if s.files == nil {
+    return nil
+  }
+
+  return s.files
 }
 
 // Less() compares a SizeTreeEntry to another SizeTreeEntry 
--- internal/sizetree/sizetree_test.go
+++ internal/sizetree/sizetree_test.go
@@ -1,15 +1,119 @@
 package sizetree_test
 
 import (
+	// "fmt"
 	"testing"
 
 	"github.com/jessegalley/fsdupscan/internal/sizetree"
 	"github.com/stretchr/testify/assert"
 )
 
-
+// Test initialization of the SizeTree
 func TestNewSizeTree(t *testing.T) {
   st := sizetree.New()
   
   assert.IsType(t, &sizetree.SizeTree{}, st)
 }
+
+// Test insertion of a SizeTreeEntry into the SizeTree 
+// for both existing and non existing sizes.
+func TestReplaceOrInsert(t *testing.T) {
+  st := sizetree.New()
+
+  e := sizetree.NewSizeTreeEntry(1000, []string{"/test/file/one"})
+  node := st.ReplaceOrInsert(e) 
+  assert.Nil(t, node)
+
+  e2 := sizetree.NewSizeTreeEntry(1000, []string{"/test/file/two"})
+  node2 := st.ReplaceOrInsert(e2)
+  assert.Same(t, e, node2)
+}
+
+// Test getting of existing and non existing SizeTreeEntries from
+// the SizeTree 
+func TestGet(t *testing.T) {
+  st := sizetree.New()
+
+  e1 := sizetree.NewSizeTreeEntry(1000, []string{"/test/file/one"})
+  item1 := st.ReplaceOrInsert(e1)
+  assert.Nil(t, item1)
+
+  e2 := sizetree.NewSizeTreeEntry(1000, nil)
+  item2 := st.Get(e2)
+  assert.Same(t, item2, e1)
+
+  e3 := sizetree.NewSizeTreeEntry(1001, nil)
+  item3 := st.Get(e3)
+  assert.Nil(t, item3)
+}
+
+// Test getting of existing and non existing SizeTreeEntries by Size alone
+func TestGetBySize(t *testing.T) {
+  st := sizetree.New()
+
+  e1 := sizetree.NewSizeTreeEntry(1000, []string{"/test/file/one"})
+  item1 := st.ReplaceOrInsert(e1)
+  assert.Nil(t, item1)
+  
+  item2 := st.GetBySize(1000)
+  assert.Same(t, e1, item2)
+
+  item3 := st.GetBySize(1001)
+  assert.Nil(t, item3)
+}
+
+// Test appending an additional filename to a SizeTreeEntry
+func TestAppend(t *testing.T) {
+  // st := sizetree.New()
+
+  e1 := sizetree.NewSizeTreeEntry(1000, []string{"/file/one"})
+  // item1 := st.ReplaceOrInsert(e1)
+  // assert.Nil(t, item1)
+
+  e1.Append("/test/two")
+
+  files := e1.Files()
+  assert.Equal(t, len(files), 2)
+
+  assert.Equal(t, files[0], "/file/one")
+  assert.Equal(t, files[1], "/test/two")
+}
+
+// tests merging two SizeTreeEntry's files into one 
+func TestMerge(t *testing.T) {
+  e1 := sizetree.NewSizeTreeEntry(1000, []string{"/file/one"})
+  e2 := sizetree.NewSizeTreeEntry(1000, []string{"/test/two"})
+
+  e1.Merge(e2)
+  files := e1.Files()
+  assert.Equal(t, len(files), 2)
+
+  assert.Equal(t, files[0], "/file/one")
+  assert.Equal(t, files[1], "/test/two")
+}
+
+func TestMergOrInsert(t *testing.T) {
+  st := sizetree.New()
+
+  e1 := sizetree.NewSizeTreeEntry(1000, []string{"/file/one"})
+  item1 := st.MergeOrInsert(e1)
+  assert.Nil(t, item1)
+
+  e2 := sizetree.NewSizeTreeEntry(1000, []string{"/test/two"})
+  item2 := st.MergeOrInsert(e2)
+  assert.IsType(t, &sizetree.SizeTreeEntry{}, item2)
+
+  files := item2.Files()
+  assert.Equal(t, len(files), 2)
+  assert.Equal(t, files[0], "/file/one")
+  assert.Equal(t, files[1], "/test/two")
+
+  // fmt.Println(item2)
+  // fmt.Println(st.GetBySize(1000))
+}
+
+
+
+
+
+
