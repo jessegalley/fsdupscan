670571dd241230b3536d53c26d6748d25edea701
jesse galley <jesse.galley@gmail.com>
2024-07-06 11:10:00 -0300

    added TestFileChecksum

--- internal/filechecksum/filechecksum_test.go
+++ internal/filechecksum/filechecksum_test.go
@@ -1,12 +1,21 @@
 package filechecksum_test
 
 import (
+	// "fmt"
 	"testing"
 
+	"github.com/jessegalley/fsdupscan/internal/filechecksum"
 	"github.com/stretchr/testify/assert"
 )
 
 
 func TestFileChecksum(t *testing.T) {
   assert.Equal(t, 1,1)
+  file := "../../testdata/file4_9K.bin"
+  checksum, err := filechecksum.CalculateChecksum(file)
+  if err != nil {
+    t.Fatalf("checksum calc failed on %v with %v", file, err)
+  }
+  // fmt.Println(checksum)
+  assert.Equal(t, checksum, "1f9d7ebdff2be849d10147e7619245c882f94a39b60ebf5f798b9e3c5154a5a7")
 }
